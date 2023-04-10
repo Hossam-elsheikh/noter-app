889119e51a29cd50c7962ec80b5b4c1a8a9e0e27
Hossam <hossam.m.elsheikh@gmail.com>
2023-04-11 01:34:09 +0200

    note edit

--- src/Components/Noter/Note.js
+++ src/Components/Noter/Note.js
@@ -1,28 +1,48 @@
-import React from 'react'
-import Card from '../UI/Card'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
-import { useDispatch } from 'react-redux'
-import { deleteNote } from './notesSlice'
-import classes from './Note.module.css'
-const Note = ({title, note,id}) => {
-  const dispatch = useDispatch()
-  const deleteNoteHandler =() =>{
-    dispatch(deleteNote(id))
-  }
+import Card from "../UI/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch } from "react-redux";
+import { deleteNote } from "./notesSlice";
+import { editNote,editTitle } from "./notesSlice";
+import classes from "./Note.module.css";
+const Note = ({ title, note, id }) => { 
+  const dispatch = useDispatch();
+  const deleteNoteHandler = () => {
+    dispatch(deleteNote(id));
+  };
+  const onTitleChangeHandler = (e) => {
+    dispatch(
+      editTitle({ id, title: e.currentTarget.textContent })
+    );
+  };
+  const onNoteChangeHandler = (e) => {
+    dispatch(
+      editNote({ id, note: e.currentTarget.textContent })
+    );
+  };
   return (
-   <Card>
-   <div>
+    <Card>
+      <div className={classes.txt}>
+        <h3  contentEditable onBlur={onTitleChangeHandler}>
+          {title}
+        </h3>
+        <p
+          contentEditable
+          onBlur={onNoteChangeHandler}
+        >
+          {note}
+        </p>
+      </div>
+      <div className={classes.icons}>
+        <FontAwesomeIcon
+          icon={faTrash}
+          onClick={deleteNoteHandler}
+          size="sm"
+          title="delete"
+        />
+      </div>
+    </Card>
+  );
+};
 
-    <h3>{title}</h3>
-    <p>{note}</p>
-   </div>
-   <div className={classes.icons}>
-    <FontAwesomeIcon icon={faTrash} onClick={deleteNoteHandler} size='sm' title='delete'/>
-   </div>
-    
-   </Card>
-  )
-}
-
-export default Note
+export default Note;
--- src/Components/Noter/notesSlice.js
+++ src/Components/Noter/notesSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState ={
-    notes:[{title:'note1',note:'this is my first note'}]
+    notes:[{title:'note1',note:'this is my first note',id:'sam'}]
 }
 
 export const notesSlice = createSlice({
@@ -13,9 +13,18 @@ export const notesSlice = createSlice({
         },
         deleteNote : (state,action) =>{
             state.notes = state.notes.filter((n)=> n.id !== action.payload) 
+        },
+        editNote : (state,action) =>{
+            const indexOfNote = state.notes.findIndex((n)=> n.id === action.payload.id)
+            
+            state.notes[indexOfNote] = {...state.notes[indexOfNote], note: action.payload.note}
+        },
+        editTitle : (state,action) =>{
+            const indexOfNote = state.notes.findIndex((n)=> n.id === action.payload.id)
+            state.notes[indexOfNote] = {...state.notes[indexOfNote], title: action.payload.title}
         }
     }
 })
 
-export const {addNote,deleteNote} = notesSlice.actions;
+export const {addNote,deleteNote,editNote,editTitle} = notesSlice.actions;
 export default notesSlice.reducer 
